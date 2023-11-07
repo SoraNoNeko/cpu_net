8494c544466304909388d7e5c71a6170eb89e5fa
SoraNoNeko <1624467507@qq.com>
2023-11-08 06:02:45 +0800

    UI基本框架修改

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,27 @@ namespace cpu_net
         {
             InitializeComponent();
         }
+        // 页面实例的缓存
+        private static readonly Dictionary<Type, Page> bufferedPages =
+            new Dictionary<Type, Page>();
+        // 当
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // 如果选择项不是 ListBoxItem, 则返回
+            if (PageMenu.SelectedItem is not ListBoxItem item)
+                return;
+
+            // 如果 Tag 不是一个类型, 则返回
+            if (item.Tag is not Type type)
+                return;
+
+            // 如果页面缓存中找不到页面, 则创建一个新的页面并存入
+            if (!bufferedPages.TryGetValue(type, out Page? page))
+                page = bufferedPages[type] =
+                    Activator.CreateInstance(type) as Page ?? throw new Exception("this would never happen");
+
+            // 使用 Frame 进行导航.
+            PageFrame.Navigate(page);
+        }
     }
 }
