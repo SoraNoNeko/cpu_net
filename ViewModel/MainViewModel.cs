dad4462398810fac3e64167c809a3e8cdd4a0002
SoraNoNeko <1624467507@qq.com>
2023-11-19 13:01:22 +0800

    优化连接CPU时的IP判断，放弃了询问DNS服务器的判断方式

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -212,26 +212,8 @@ namespace cpu_net.ViewModel
                     $"&upass={settingData.Password}&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1.3&v=7011&lang=zh";
                         break;
                     case 1:
-                        IPHostEntry ipEntry;
                         string local_ip;
-                        try
-                        {
-                            ipEntry = Dns.GetHostEntry("192.168.199.54");
-                            local_ip = ipEntry.ToString();
-                        }
-                        catch
-                        {
-                            try
-                            {
-                                ipEntry = Dns.GetHostEntry("172.17.253.5");
-                                local_ip = ipEntry.ToString();
-                            }
-                            catch
-                            {
-                                local_ip = _IP;
-                            }
-                        }
-                        Info(local_ip);
+                        local_ip = _IP;
                         Login_url = $"http://192.168.199.21:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=%2C1%2C{settingData.Username}&user_password={settingData.Password}" +
                             $"&wlan_user_ip={local_ip}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.3&v=1954";
                         break;
